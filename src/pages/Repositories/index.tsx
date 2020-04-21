20229292ce372a8789f3c83d887779b843cc6737
RenanArantes <renan.arantes@rede.ulbra.br>
2020-04-21 08:33:43 -0300

    estilizando detalhe

--- src/pages/Repositories/index.tsx
+++ src/pages/Repositories/index.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/47373024?s=460&u=8afa3f82edb0f4bd447877a1f33cd108d47548b7&v=4"
+            alt="Avatar"
+          />
+          <div>
+            <strong>renanarantes/gobarber</strong>
+            <p>descricao do repositorio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="sadasd">
+          <div>
+            <strong>Nome do repositorio</strong>
+            <p>Descricao do repositorio</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
